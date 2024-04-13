9567e4e801043aa77ed90bd4c4d60cac2d3777a9
Ram Bagga <rambagga.gb@gmail.com>
2024-04-13 18:15:14 -0400

    Responding to PR comments.
    1) Refactoring code in api.go
    2) Fixing default debuglevel in sample-btcd.conf

--- api.go
+++ api.go
@@ -5,76 +5,62 @@ import (
 	"os/exec"
 )
 
-func btcctlCommand(args []string) {
+// btcctlConfPath is the path to the btcctl configuration file
+const btcctlConfPath = "C:/Users/ANJALI JADEJA/$GOPATH/orcacointest/cmd/btcctl/sample-btcctl.conf"
+
+// Commands
+const (
+	getBalanceCommand       = "getbalance"
+	sendToAddressCommand    = "sendtoaddress"
+	generateCommand         = "generate"
+	walletPassphraseCommand = "walletpassphrase"
+)
+
+func main() {
+	args := []string{getBalanceCommand}
+	apiCall(args)
+}
+
+func apiCall(args []string) {
 	argsLen := len(args)
 	if argsLen == 0 {
 		fmt.Println("No commands were provided")
 		return
 	}
 
-	btcctlConfPath := "/Users/draku/go/src/orcacointest/cmd/btcctl/sample-btcctl.conf"
-	argIdx := 0
-	for argIdx < argsLen {
+	for argIdx := 0; argIdx < argsLen; argIdx++ {
 		arg := args[argIdx]
 		switch arg {
-		case "getbalance":
-			cmd := exec.Command("btcctl", "--configfile="+btcctlConfPath, "getbalance", "--notls")
-			output, err := cmd.Output()
+		case getBalanceCommand:
+			err := executeCommand(getBalanceCommand)
 			if err != nil {
 				fmt.Println("Error when running 'getbalance':", err)
 				return
 			}
-
-			fmt.Println(string(output))
-		case "sendtoaddress":
+		case sendToAddressCommand, generateCommand, walletPassphraseCommand:
 			if len(args[argIdx:]) < 2 {
-				fmt.Print("Not enough arguments for the command 'sendtoaddress'")
+				fmt.Printf("Not enough arguments for the command '%s'\n", arg)
 				return
 			}
 
-			cmd := exec.Command("btcctl", "--configfile="+btcctlConfPath, "sendtoaddress",
-				args[argIdx+1], args[argIdx+2], "--notls")
-			output, err := cmd.Output()
+			err := executeCommand(arg, args[argIdx+1:]...)
 			if err != nil {
-				fmt.Println("Error running 'sendtoaddress'", err)
+				fmt.Printf("Error running '%s': %v\n", arg, err)
 				return
 			}
-
-			argIdx += 2
-			fmt.Println(string(output))
-		case "generate":
-			if len(args[argIdx:]) < 1 {
-				fmt.Print("Not enough arguments for the command 'generate'")
-				return
-			}
-
-			cmd := exec.Command("btcctl", "--configfile="+btcctlConfPath, "generate", args[argIdx+1], "--notls")
-			output, err := cmd.Output()
-			if err != nil {
-				fmt.Println("Error running 'generate'", err)
-				return
-			}
-
-			argIdx += 1
-			fmt.Println(string(output))
-		case "walletpassphrase":
-			if len(args[argIdx:]) < 2 {
-				fmt.Print("Not enough arguments for the command 'walletpassphrase'")
-				return
-			}
-
-			cmd := exec.Command("btcctl", "--configfile="+btcctlConfPath, "walletpassphrase",
-				args[argIdx+1], args[argIdx+2], "--notls")
-			output, err := cmd.Output()
-			if err != nil {
-				fmt.Println("Error running 'walletpassphrase'", err)
-				return
-			}
-
-			argIdx += 2
-			fmt.Println(string(output))
+			argIdx++
 		}
-
-		argIdx += 1
 	}
+}
+
+// executeCommand executes the given command with the provided arguments
+func executeCommand(command string, args ...string) error {
+	cmd := exec.Command("btcctl", append([]string{"--configfile=" + btcctlConfPath, command}, args...)...)
+	cmd.Args = append(cmd.Args, "--notls")
+	output, err := cmd.Output()
+	if err != nil {
+		return fmt.Errorf("error running '%s': %w", command, err)
+	}
+	fmt.Println(string(output))
+	return nil
 }
