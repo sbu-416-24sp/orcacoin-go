b271732c23fd0d4ce118522a8d700a20c8074624
Ram Bagga <rambagga.gb@gmail.com>
2024-04-21 00:23:37 -0400

    Updates to the the api.go file, and increasing difficulty of mining.
     - Udpates to the api file so we can run btcd and wallet in the background all via this file.

--- api.go
+++ api.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"sync"
+	"time"
 )
-
-// btcctlConfPath is the path to the btcctl configuration file
-const btcctlConfPath = "C:/Users/ANJALI JADEJA/$GOPATH/orcacointest/cmd/btcctl/sample-btcctl.conf"
 
 // Commands
 const (
@@ -17,8 +16,24 @@ const (
 )
 
 func main() {
+	// Create WaitGroup to wait for both commands to finish
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go runBtcd()
+
+	wg.Add(1)
+	go runBtcWallet()
+
+	// calls the getbalance from btcctl after waiting for 10 seconds to ensure wallet connects
+	time.Sleep(10 * time.Second)
 	args := []string{getBalanceCommand}
 	apiCall(args)
+
+	// Wait for both commands to finish
+	wg.Wait()
+
+	fmt.Println("Both btcd and btcwallet have finished running.")
 }
 
 func apiCall(args []string) {
@@ -34,7 +49,6 @@ func apiCall(args []string) {
 		case getBalanceCommand:
 			err := executeCommand(getBalanceCommand)
 			if err != nil {
-				fmt.Println("Error when running 'getbalance':", err)
 				return
 			}
 		case sendToAddressCommand, generateCommand, walletPassphraseCommand:
@@ -45,7 +59,6 @@ func apiCall(args []string) {
 
 			err := executeCommand(arg, args[argIdx+1:]...)
 			if err != nil {
-				fmt.Printf("Error running '%s': %v\n", arg, err)
 				return
 			}
 			argIdx++
@@ -55,12 +68,35 @@ func apiCall(args []string) {
 
 // executeCommand executes the given command with the provided arguments
 func executeCommand(command string, args ...string) error {
-	cmd := exec.Command("btcctl", append([]string{"--configfile=" + btcctlConfPath, command}, args...)...)
-	cmd.Args = append(cmd.Args, "--notls")
+	cmd := exec.Command("btcctl", command)
+	cmd.Args = append(cmd.Args, args...)
+	// cmd.Args = append(cmd.Args, "--notls")
 	output, err := cmd.Output()
 	if err != nil {
-		return fmt.Errorf("error running '%s': %w", command, err)
+		return fmt.Errorf("Error running '%s': %w", command, err)
 	}
 	fmt.Println(string(output))
+	return nil
+}
+
+func runBtcd() error {
+	cmd := exec.Command("btcd")
+
+	err := cmd.Start()
+	if err != nil {
+		return fmt.Errorf("Error starting btcd: %v", err)
+	}
+
+	return nil
+}
+
+func runBtcWallet() error {
+	cmd := exec.Command("btcwallet")
+
+	err := cmd.Start()
+	if err != nil {
+		return fmt.Errorf("Error starting btcwallet: %v", err)
+	}
+
 	return nil
 }
--- chaincfg/params.go
+++ chaincfg/params.go
@@ -29,7 +29,7 @@ var (
 
 	// freshNetPowLimit is the highest proof of work value a Bitcoin block
 	// can have for the fresh network.  It is the value 2^224 - 1.
-	freshNetPowLimit = regressionPowLimit
+	freshNetPowLimit = mainPowLimit
 
 	// regressionPowLimit is the highest proof of work value a Bitcoin block
 	// can have for the regression test network.  It is the value 2^255 - 1.
@@ -437,25 +437,22 @@ var FreshNetParams = Params{
 	// need to define a port num so that peers can communicate in different networks by
 	// looking at the port number
 	DefaultPort: "41600",
-	// TODO: seed nodes left blank for now
-	DNSSeeds: []DNSSeed{
-		{"ns1.orca.com", true},
-	},
+	// Leaving Seed nodes blank for now
+	DNSSeeds: []DNSSeed{},
 
 	// Chain parameters
 	GenesisBlock: &freshNetGenesisBlock,
 	GenesisHash:  &freshNetGenesisHash,
 	PowLimit:     freshNetPowLimit,
 	// determines how difficult to mine
-
-	PowLimitBits:  0x207fffff,
+	PowLimitBits:  0x1d00ffff,
 	BIP0034Height: 227931, // 000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8
 	BIP0065Height: 388381, // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
 	BIP0066Height: 363725, // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
 	// defines the number of confirmations required before newly mined coins can be spent.
 	// Lowering this value reduces the waiting time for miners to spend their rewards, which can incentivize mining
 	CoinbaseMaturity:         1,
-	SubsidyReductionInterval: 150,
+	SubsidyReductionInterval: 210000,
 	// widens the window for difficulty adjustments.
 	TargetTimespan: time.Hour * 24 * 14, // 14 days
 	// to slow down the block production rate
@@ -466,9 +463,9 @@ var FreshNetParams = Params{
 	// which can help the network quickly adapt to changes in mining power or difficulty
 	RetargetAdjustmentFactor: 4, // 25% less, 400% more
 	ReduceMinDifficulty:      true,
-	MinDiffReductionTime:     time.Minute * 20,
+	MinDiffReductionTime:     0,
 	// can use generate command from RPC to generate blocks in lieu of standard mining procedures
-	GenerateSupported: true,
+	GenerateSupported: false,
 
 	// Checkpoints ordered from oldest to newest.
 	// these are specific blocks as the chain grows to maintain stability and security
@@ -477,8 +474,8 @@ var FreshNetParams = Params{
 	// Consensus rule change deployments.
 	// The miner confirmation window is defined as:
 	//   target proof of work timespan / target proof of work spacing
-	RuleChangeActivationThreshold: 108, // 75% of MinerConfirmationWindow
-	MinerConfirmationWindow:       144,
+	RuleChangeActivationThreshold: 1916, // 95% of MinerConfirmationWindow
+	MinerConfirmationWindow:       2016, //
 
 	// can leave this as is, unless we want to change the consensus rules
 	// these are rules that everyone must follows to reach an agreeable state of the network
@@ -486,16 +483,16 @@ var FreshNetParams = Params{
 		DeploymentTestDummy: {
 			BitNumber: 28,
 			DeploymentStarter: NewMedianTimeDeploymentStarter(
-				time.Time{}, // Always available for vote
+				time.Unix(11991456010, 0), // January 1, 2008 UTC
 			),
 			DeploymentEnder: NewMedianTimeDeploymentEnder(
-				time.Time{}, // Never expires
+				time.Unix(1230767999, 0), // December 31, 2008 UTC
 			),
 		},
 		DeploymentTestDummyMinActivation: {
 			BitNumber:                 22,
-			CustomActivationThreshold: 72,  // Only needs 50% hash rate.
-			MinActivationHeight:       600, // Can only activate after height 600.
+			CustomActivationThreshold: 1815,    // Only needs 90% hash rate.
+			MinActivationHeight:       10_0000, // Can only activate after height 10k.
 			DeploymentStarter: NewMedianTimeDeploymentStarter(
 				time.Time{}, // Always available for vote
 			),
@@ -506,30 +503,31 @@ var FreshNetParams = Params{
 		DeploymentCSV: {
 			BitNumber: 0,
 			DeploymentStarter: NewMedianTimeDeploymentStarter(
-				time.Time{}, // Always available for vote
+				time.Unix(1462060800, 0), // May 1st, 2016
 			),
 			DeploymentEnder: NewMedianTimeDeploymentEnder(
-				time.Time{}, // Never expires
+				time.Unix(1493596800, 0), // May 1st, 2017
 			),
 		},
 		DeploymentSegwit: {
 			BitNumber: 1,
 			DeploymentStarter: NewMedianTimeDeploymentStarter(
-				time.Time{}, // Always available for vote
+				time.Unix(1479168000, 0), // November 15, 2016 UTC
 			),
 			DeploymentEnder: NewMedianTimeDeploymentEnder(
-				time.Time{}, // Never expires.
+				time.Unix(1510704000, 0), // November 15, 2017 UTC.
 			),
 		},
 		DeploymentTaproot: {
 			BitNumber: 2,
 			DeploymentStarter: NewMedianTimeDeploymentStarter(
-				time.Time{}, // Always available for vote
+				time.Unix(1619222400, 0), // April 24th, 2021 UTC.
 			),
 			DeploymentEnder: NewMedianTimeDeploymentEnder(
-				time.Time{}, // Never expires.
+				time.Unix(1628640000, 0), // August 11th, 2021 UTC.
 			),
-			CustomActivationThreshold: 108, // Only needs 75% hash rate.
+			CustomActivationThreshold: 1815, // 90%
+			MinActivationHeight:       709_632,
 		},
 	},
 
